a8b1e332a5031690269c5de627ca0c09e42134a0
bearbugwewe <aofmonkychat1979@hotmail.com>
2025-05-01 20:26:14 +0700

    Update course-create.js

--- utw-modify-api/controllers/course-create.js
+++ utw-modify-api/controllers/course-create.js
@@ -17,7 +17,7 @@ module.exports = async function (req, res) {
     if (authResult !== true) {
       return; // ไม่ผ่าน auth ก็จบ
     }
-    
+
     var user_id = req.body.user_id;
     var subject_id = req.body.subject_id;
     var indicators = req.body.indicators;
@@ -25,9 +25,8 @@ module.exports = async function (req, res) {
     var subject_code = req.body.subject_code;
     var subject_class = req.body.subject_class;
 
-    if(subject_id) {
-      const [data] = await dbCors.query(
-        `SELECT 
+    if (subject_id) {
+      const [data] = await dbCors.query(`SELECT 
             subject.id AS id, 
             subject.name AS name, 
             subject.subject_code AS subject_code, 
@@ -37,16 +36,26 @@ module.exports = async function (req, res) {
             subject.created_at AS created_at,
             subject.updated_at AS updated_at
             FROM subject 
-            WHERE subject.id = ?`, 
-        [user_id]
+            WHERE subject.id = ?`,
+        [subject_id]
       );
+      console.log("data",)
       if (data.length && data[0]) {
         var department_id = data[0].department_id;
 
-         await dbGrade.query(`INSERT INTO course (subject_id, department_id, subject_title,  subject_code,  subject_class, user_id, indicators) 
+        await dbGrade.query(`INSERT INTO course (subject_id, department_id, subject_title,  subject_code,  subject_class, user_id, indicators) 
           VALUES (?, ?, ?, ?, ?, ?, ?)`,
-        [subject_id, department_id, subject_title,  subject_code,  subject_class, user_id, indicators])
-        return success(res, data);
+          [subject_id, department_id, subject_title, subject_code, subject_class, user_id, indicators]);
+
+        return success(res, [{
+          "subject_id": subject_id,
+          "department_id": department_id,
+          "subject_title": subject_title,
+          "subject_code": subject_code,
+          "subject_class": subject_class,
+          "user_id": user_id,
+          "indicators": indicators,
+        }]);
       }
       else {
         return empty(res);
